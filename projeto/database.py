1d7859451eecce904619500e4a2e27b10191e736
Luis Fillype Vilela Cunha <luis.cunha@contavoltz.com>
2023-06-26 13:29:56 -0300

    refactor: change db class to mongo

--- projeto/database.py
+++ projeto/database.py
@@ -1,21 +1,26 @@
-from neo4j import GraphDatabase
+import pymongo # pip install pymongo
 
 
 class Database:
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+    def __init__(self, database, collection):
+        self.connect(database, collection)
 
-    def close(self):
-        self.driver.close()
+    def connect(self, database, collection):
+        try:
+            connectionString = "mongodb+srv://root:root@batata.0ldpz6w.mongodb.net/test"
+            self.clusterConnection = pymongo.MongoClient(
+                connectionString,
+                tlsAllowInvalidCertificates=True
+            )
+            self.db = self.clusterConnection[database]
+            self.collection = self.db[collection]
+            print("Conectado ao banco de dados com sucesso!")
+        except Exception as e:
+            print(e)
 
-    def execute_query(self, query, parameters=None):
-        data = []
-        with self.driver.session() as session:
-            results = session.run(query, parameters)
-            for record in results:
-                data.append(record)
-            return data
-
-    def drop_all(self):
-        with self.driver.session() as session:
-            session.run("MATCH (n) DETACH DELETE n")
+    def resetDatabase(self):
+        try: 
+            self.db.drop_collection(self.collection)
+            print("Banco de dados resetado com sucesso!")
+        except Exception as e:
+            print(e)
